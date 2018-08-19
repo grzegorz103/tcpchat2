6e611368d1f048f3a7ca393b08ca97e5aed1be74
grzegorz103 <38592759+grzegorz103@users.noreply.github.com>
2018-08-19 20:59:03 +0200

    Update Message.java

--- src/Message.java
+++ src/Message.java
@@ -2,98 +2,98 @@ import java.io.Serializable;
 
 class Message implements Serializable
 {
-    private String message;
-    private byte[] file;
-    private boolean isValidate, nickAccepted;
-    private String recipient, sender;
-
-    private Message(MessageBuilder msgbd)
-    {
-        this.message = msgbd.message;
-        this.file = msgbd.file;
-        this.recipient = msgbd.recipient;
-        this.sender = msgbd.sender;
-        this.isValidate = msgbd.isValidate;
-        this.nickAccepted = msgbd.nickAccepted;
-
-    }
-
-    String getMessage()
-    {
-        return message;
-    }
-
-    byte[] getFile()
-    {
-        return file;
-    }
-
-    String getRecipient()
-    {
-        return recipient;
-    }
-
-    boolean getIsValidate()
-    {
-        return isValidate;
-    }
-
-    boolean getNickAccepted()
-    {
-        return nickAccepted;
-    }
-
-    String getSender()
-    {
-        return sender;
-    }
-
-    static class MessageBuilder
-    {
         private String message;
         private byte[] file;
         private boolean isValidate, nickAccepted;
         private String recipient, sender;
 
-        MessageBuilder message(String message)
+        private Message(MessageBuilder msgbd)
         {
-            this.message = message;
-            return this;
+                this.message = msgbd.message;
+                this.file = msgbd.file;
+                this.recipient = msgbd.recipient;
+                this.sender = msgbd.sender;
+                this.isValidate = msgbd.isValidate;
+                this.nickAccepted = msgbd.nickAccepted;
+
         }
 
-        MessageBuilder file(byte... file)
+        String getMessage()
         {
-            this.file = file;
-            return this;
+                return message;
         }
 
-        MessageBuilder validate()
+        byte[] getFile()
         {
-            this.isValidate = true;
-            return this;
+                return file;
         }
 
-        MessageBuilder recipient(String nick)
+        String getRecipient()
         {
-            this.recipient = nick;
-            return this;
+                return recipient;
         }
 
-        MessageBuilder sender(String nick)
+        boolean getIsValidate()
         {
-            this.sender = nick;
-            return this;
+                return isValidate;
         }
 
-        MessageBuilder acceptNick()
+        boolean getNickAccepted()
         {
-            this.nickAccepted = true;
-            return this;
+                return nickAccepted;
         }
 
-        Message build()
+        String getSender()
         {
-            return new Message(this);
+                return sender;
         }
-    }
+
+        static class MessageBuilder
+        {
+                private String message;
+                private byte[] file;
+                private boolean isValidate, nickAccepted;
+                private String recipient, sender;
+
+                MessageBuilder message(String message)
+                {
+                        this.message = message;
+                        return this;
+                }
+
+                MessageBuilder file(byte... file)
+                {
+                        this.file = file;
+                        return this;
+                }
+
+                MessageBuilder validate()
+                {
+                        this.isValidate = true;
+                        return this;
+                }
+
+                MessageBuilder recipient(String nick)
+                {
+                        this.recipient = nick;
+                        return this;
+                }
+
+                MessageBuilder sender(String nick)
+                {
+                        this.sender = nick;
+                        return this;
+                }
+
+                MessageBuilder acceptNick()
+                {
+                        this.nickAccepted = true;
+                        return this;
+                }
+
+                Message build()
+                {
+                        return new Message(this);
+                }
+        }
 }
